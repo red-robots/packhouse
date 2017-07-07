22116752c04590a8be464116eaec1e205b3f6620
Austin Crane <austincrane@gmail.com>
2017-07-07 13:29:49 -0400

    add favicon

--- header.php
+++ header.php
@@ -19,11 +19,11 @@
 <link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700|Oswald:300,400" rel="stylesheet">
 <script src="https://use.fontawesome.com/762c66dd2b.js"></script>
 
-<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/icons/favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/icons/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_url'); ?>/icons/favicons/favicon-16x16.png">
-<link rel="manifest" href="<?php bloginfo('template_url'); ?>/icons/favicons/manifest.json">
-<link rel="mask-icon" href="<?php bloginfo('template_url'); ?>/icons/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/images/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/images/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_url'); ?>/images/favicons/favicon-16x16.png">
+<link rel="manifest" href="<?php bloginfo('template_url'); ?>/images/favicons/manifest.json">
+<link rel="mask-icon" href="<?php bloginfo('template_url'); ?>/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
 <meta name="theme-color" content="#ffffff">
 
 <?php wp_head(); 
